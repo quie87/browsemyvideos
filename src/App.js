d43dbedbb9375887e95808a89f3cf12068fff690
Kristoffer Åberg <kristoffer.aberg12@gmail.com>
2021-11-27 16:14:13 +0100

    create the footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import { React } from "react";
 import Main from "./components/Main/Main";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
