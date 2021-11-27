bc2553ab8ca76aaa457cfcc02eff89e8464cd2f9
Kristoffer Åberg <kristoffer.aberg12@gmail.com>
2021-11-27 16:37:06 +0100

    fix the eternal loop and display title of first movie

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,20 +1,32 @@
-import { React } from "react";
+import { React, useState, useEffect } from "react";
 
 function Main() {
   const api_key = process.env.REACT_APP_API_KEY;
 
-  const fetchMovies = async () => {
-    const res = await fetch(
-      `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=1`
-    );
+  const [movies, setMovies] = useState([]);
 
-    const data = await res.json();
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const res = await fetch(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=1`
+      );
 
-    console.log(data);
-  };
+      const data = await res.json();
 
-  fetchMovies();
-  return <div className="Main"></div>;
+      console.log(data);
+      setMovies(data.results);
+    };
+
+    fetchMovies();
+  }, [api_key]);
+
+  return (
+    <main className="Main">
+      <div>
+        <h2>{movies[0].title}</h2>
+      </div>
+    </main>
+  );
 }
 
 export default Main;
